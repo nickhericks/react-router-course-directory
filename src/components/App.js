20b968c767399d2468a62d677185b13ddeea9c44
Nick-Hericks <nickhericks@gmail.com>
2019-05-09 05:08:04 +0200

    Add URL parameters for Featured component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {
 	BrowserRouter,
 	Route,
-	Switch
+	Switch,
 } from 'react-router-dom';
 
 // App components
@@ -23,7 +23,7 @@ const App = () => (
 				<Route exact path="/" component={Home}/>
 				<Route path="/about" render={ () => <About title='About' /> }/>
 				<Route exact path="/teachers" component={Teachers}/>
-				<Route path="/teachers/:name" component={Featured}/>
+				<Route path="/teachers/:topic/:fname-:lname" component={Featured}/>
 				<Route path="/courses" component={Courses}/>
 				<Route component={NotFound} />
 			</Switch>
--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Featured = ({ match }) => {
-	let name = match.params.name;
+	let name = `${match.params.fname} ${match.params.lname}`;
+	let topic = match.params.topic;
   return (
     <div className="main-content">
       <h2>{name} </h2>
-      <p>Introducing <strong>{name}</strong>, a teacher who loves teaching courses about <strong></strong>!</p>
+      <p>Introducing <strong>{name}</strong>, a teacher who loves teaching courses about <strong>{topic}</strong>!</p>
     </div>
   );
 }
